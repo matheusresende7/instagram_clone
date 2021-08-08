4a60eae296431516083cdded6cfa6bac773fae54
matheusresende7 <matheusresende1995@hotmail.com>
2021-08-08 00:55:57 -0300

    chore: add modular arch

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,7 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/src/app/app_widget.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(AppWidget());
+}
